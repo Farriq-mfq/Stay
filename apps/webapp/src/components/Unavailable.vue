ba746b5bf9c2a580534f442d722c31f5e0287cef
Farriq muwaffaq <farriqmuwaffaq100@gmail.com>
2025-04-06 14:40:36 +0700

    chore: fix unavailable

--- apps/webapp/src/components/Unavailable.vue
+++ apps/webapp/src/components/Unavailable.vue
@@ -1,7 +1,7 @@
 <template>
     <div
-        class="absolute top-0 right-0 surface-ground opacity-40 w-full h-full flex align-items-center justify-content-center">
-        <p class="text-center text-white">
+        class="absolute top-0 right-0 surface-900 opacity-70 w-full h-full flex align-items-center justify-content-center">
+        <p class="text-center text-primary">
             <i class="pi pi-lock text-sm mr-2"></i>
             <span class="text-sm">
                 Fitur ini belum tersedia
